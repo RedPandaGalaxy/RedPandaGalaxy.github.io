fb160e9851a63a14e33799c19105662aeb752498
RedPandaGalaxy <wiktoria98.1998@o2.pl>
2025-01-11 21:32:48 +0100

    Make carousel function support multiple carousels

--- app.js
+++ app.js
@@ -10,28 +10,44 @@ window.addEventListener("scroll", () => {
 })
 
 // SLIDE SHOW
-let slideIndex = 1;
-showSlide(slideIndex);
+const slideIndex = {
+    c0: 1,
+    c1: 1,
+    c2: 1,
+    c3: 1,
+};
+showSlide(slideIndex, "c0", "dot-c0");
+
+// let slideIndexC1 = 2;
+// showSlide(slideIndexC1, "c1", "dot-c1");
+
+// let slideIndexC2 = 3;
+// showSlide(slideIndexC2, "c2", "dot-c2");
+
+// let slideIndexC3 = 4;
+// showSlide(slideIndexC3, "c3", "dot-c3");
 
 // Next/previous controls
-function plusSlide(n) {
-    showSlide(slideIndex += n);
+function plusSlide(n, carousel, dot) {
+    showSlide(slideIndex[carousel] += n, carousel, dot);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlide(slideIndex = n);
+function currentSlide(n, carousel, dot) {
+    showSlide(slideIndex[carousel] = n, carousel, dot);
 }
 
-function showSlide(n) {
+function showSlide(n, carousel, dot) {
     let i;
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName(carousel);
+    let dots = document.getElementsByClassName(dot);
+    slideIndex[carousel]
+
     if (n > slides.length) {
-        slideIndex = 1;
+        slideIndex[carousel] = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length;
+        slideIndex[carousel] = slides.length;
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -39,6 +55,7 @@ function showSlide(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-} 
+    slides[slideIndex[carousel] - 1].style.display = "block";
+    dots[slideIndex[carousel] - 1].className += " active";
+    console.log(slideIndex[carousel]);
+}
