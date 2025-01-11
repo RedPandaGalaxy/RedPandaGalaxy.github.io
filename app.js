728117b0ad022c21d1184f0a3f649b55b740bb60
RedPandaGalaxy <wiktoria98.1998@o2.pl>
2025-01-11 12:23:41 +0100

    wip-karuzela

--- app.js
+++ app.js
@@ -8,3 +8,37 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("sticky");
     }
 })
+
+// SLIDE SHOW
+let slideIndex = 1;
+showSlide(slideIndex);
+
+// Next/previous controls
+function plusSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+} 
